910b61e5b5ee5614c68f05b0eb842fa28f0a4252
aburizal10 <09021281419125@students.ilkom.unsri.ac.id>
2015-11-15 14:13:00 +0700

    Nambah Database pakai (connect.php) Ubah hello.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $hostname = "br-cdbr-azure-south-a.cloudapp.net";
 $username = "b81932d30ec9c1";
 $password = "38bb623d";
-$database_name = "Workshop Azure";
+$database_name = "WorkshopAzure";
 
 $conn = mysql_connect($hostname, $username, $password) 
 	or die ("Error menghubungkan ke host database");
